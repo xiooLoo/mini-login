7133a243d6c21b2dd5fd6b376583822d613605d0
xiaoLoo <204138282@qq.com>
2019-05-15 21:52:03 +0800

    完成登录UI

--- pages/login/login.js
+++ pages/login/login.js
@@ -6,13 +6,15 @@ Page({
    */
   data: {
     img: '../../assets/square.png',
-    isrmb: false
+    eye: '../../assets/eye_close.png',
+    isrmb: false,
+    ispwd: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     wx.setNavigationBarTitle({
       title: '登录'
     })
@@ -34,6 +36,24 @@ Page({
     }
   },
 
+  handleEye() {
+    var ispwd = !this.data.ispwd
+    this.setData({
+      ispwd: ispwd
+    })
+
+    if (ispwd === true) {
+      this.setData({
+        eye: '../../assets/eye.png'
+      })
+      return
+    }
+    this.setData({
+      eye: '../../assets/eye_close.png'
+    })
+    return
+  },
+
 
 
 
@@ -41,49 +61,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
